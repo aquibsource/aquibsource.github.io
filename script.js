c53aaa1082b48147645b840fa816fd62f1263c21
Aquib Ansari <78820089+aquibsource@users.noreply.github.com>
2023-09-23 03:24:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function calculateBudget() {
+    // Fetch Values from Input Fields
+    const startDate = new Date(document.getElementById("startDate").value);
+    const endDate = new Date(document.getElementById("endDate").value);
+    const remainingBudget = parseFloat(document.getElementById("remainingBudget").value);
+    
+    // Calculate Remaining Days
+    const oneDay = 24 * 60 * 60 * 1000; // Hours*minutes*seconds*milliseconds
+    const remainingDays = Math.round((endDate - startDate) / oneDay) + 1;  // Add 1 to include the start day
+
+    // Calculate Per Day Budget
+    const perDayBudget = remainingBudget / remainingDays;
+
+    // Display Results
+    document.getElementById("resultRemainingDays").textContent = remainingDays;
+    document.getElementById("resultPerDayBudget").textContent = perDayBudget.toFixed(2); // Rounded to 2 decimal places
+}
